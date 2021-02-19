e50a5a49e2bdd2b05d223e2f7c0da1259be3a1e7
MiracleOluwaseyi <48574483+MiracleOluwaseyi@users.noreply.github.com>
2021-02-19 08:46:14 +0100

    Shape Display Box

---
+++ BoxShape.java
@@ -0,0 +1,19 @@
+public class BoxShape{
+	
+	public static void main(String args []){
+
+System.out.println("***************");
+System.out.println("*             *");
+System.out.println("*             *");
+System.out.println("*             *");
+System.out.println("*             *");
+System.out.println("*             *");
+System.out.println("*             *");
+System.out.println("*             *");
+System.out.println("*             *");
+System.out.println("*             *");
+System.out.println("*             *");
+System.out.println("*             *");
+System.out.println("***************");
+	}
+}
