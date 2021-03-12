579dff94c08b73754904d03401846c50ef2e2516
MiracleOluwaseyi <48574483+MiracleOluwaseyi@users.noreply.github.com>
2021-03-12 07:37:30 +0100

    Add files via upload
    Jpanel Practice

---
+++ Practical.java
@@ -0,0 +1,10 @@
+package Practice;
+
+import javax.swing.*;
+
+public class Practical {
+           public static void main(String[] args){
+               JOptionPane.showMessageDialog(null, "Welcome to Java");
+             }
+        }
+
