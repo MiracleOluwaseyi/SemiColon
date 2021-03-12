28e4e8276df80f311088c8aa42755b23d2218014
MiracleOluwaseyi <48574483+MiracleOluwaseyi@users.noreply.github.com>
2021-03-12 07:40:49 +0100

    Add files via upload
    Grade Calculate Test

---
+++ KataTest.java
@@ -0,0 +1,16 @@
+package bankingApplication;
+
+import Semicolon.Africa.Kata;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KataTest {
+
+    public static void main(String[] args) {
+
+        Kata kata = new Kata();
+
+        System.out.println("Your Grade is : "  + kata.calculateGrade(23));
+    }
+
+}
